7c607a776fc3f6891c7ab87b2c13c76d278f7178
quin0304 <97483262+quin0304@users.noreply.github.com>
2022-04-15 16:43:07 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,13 @@
 let dc = document.querySelector('.dynamic-content'); //dom targets dynamic container
 let error = document.querySelector('.error');
-// let navItems = document.querySelectorAll('btn a nav'); //dom
 const resources = {
     btns: document.querySelectorAll('nav a'),
     dc: document.querySelector('.dynamic-content')
 }
-// console.log(resources.btns, resources.dc);
 
 let url = './partials/home.html';
 
 let promise = (url) =>{
-
     fetch(url)
     .then(response =>{
         if(response.statusText === 'OK'){
