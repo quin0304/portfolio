c71946ea95e1300a9376795e0717ec0261e70717
quin0304 <97483262+quin0304@users.noreply.github.com>
2022-04-15 17:26:46 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 let dc = document.querySelector('.dynamic-content'); //dom targets dynamic container
 let error = document.querySelector('.error');
+// let navItems = document.querySelectorAll('btn a nav'); //dom
 const resources = {
     btns: document.querySelectorAll('nav a'),
     dc: document.querySelector('.dynamic-content')
 }
+// console.log(resources.btns, resources.dc);
 
 let url = './partials/home.html';
 
 let promise = (url) =>{
+
     fetch(url)
     .then(response =>{
         if(response.statusText === 'OK'){
@@ -25,7 +28,7 @@ let promise = (url) =>{
 }
 promise(url);
 
-
+resources.dc.innerHTML = promise(url);
 
 function targetContent(ev) {
     ev.preventDefault();
@@ -39,12 +42,6 @@ function targetContent(ev) {
     activeItem.id='active';
     activeItem.url= ev.target.href;
     url = activeItem.url;
-    resources.dc.innerHTML = promise(url);
-    
+    promise(url);
    
 };
-
-
-for (let btn of resources.btns){
-btn.addEventListener('click', targetContent);
-}
