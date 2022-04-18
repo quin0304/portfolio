9fc400808542f882915992be2883a875c6d2e872
quin0304 <97483262+quin0304@users.noreply.github.com>
2022-04-18 07:39:23 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,24 @@
-let dc = document.querySelector('.dynamic-content'); //dom targets dynamic container
+let dc = document.querySelector('.dynamic-content');
 let error = document.querySelector('.error');
 const resources = {
     btns: document.querySelectorAll('nav a'),
     dc: document.querySelector('.dynamic-content')
 }
 
-let url = '/portfolio/partials/home.html';
+let url = './partials/home.html';
 
 let promise = (url) =>{
-
     fetch(url)
-    .then(response =>{
+    .then(response => {
         if(response.statusText === 'OK'){
             return response.text();
         }
-        //error message
         throw new Error(response.statusText);
     })
     .then (data => dc.innerHTML = data)
     .then (error => dc.innerHTML = error)
 
-.catch(err =>{
+.catch(err => {
     error.innerText = err.message;
 });
 }
@@ -40,11 +38,8 @@ function targetContent(ev) {
     activeItem.id='active';
     activeItem.url= ev.target.href;
     url = activeItem.url;
-    promise(url);
-   
+    promise(url); 
 };
-
-
 for (let btn of resources.btns){
 btn.addEventListener('click', targetContent);
 }
